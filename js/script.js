52760a58f1e61ef93cc1e239c31d61cff0b9f52a
Gabriele <grio.1743416@studenti.uniroma1.it>
2023-11-04 17:24:47 +0100

    button styles added

--- js/script.js
+++ js/script.js
@@ -4,13 +4,15 @@ function displayCounter() {
     // Create Box tag in main
     let divBox = document.createElement("div");
     divBox.className = "Box";
-    main.append(divBox);
+    container.append(divBox);
 
     // Adding all the buttons, inside divBox
-    divBox.insertAdjacentHTML(`beforeend`,`<div data-output "class="output">0</div>`);
+    divBox.insertAdjacentHTML(`beforeend`,`<div data-output>0</div>`);
     divBox.insertAdjacentHTML(`beforeend`,`<button class="all-clear">AC</button>`);
     divBox.insertAdjacentHTML(`beforeend`,`<button class="plus">+</button>`);
     divBox.insertAdjacentHTML(`beforeend`,`<button class="minus">-</button>`);
+
+  
 
     // removes startButton from html
     startButton.remove();
@@ -20,26 +22,32 @@ function displayCounter() {
 
 startButton.onclick = ()=> displayCounter();
 
-
 function thenDoThis(){
     const plusButton = document.querySelector(".plus");
     const minusButton = document.querySelector(".minus");
     const allClearButton = document.querySelector(".all-clear");
     let output = document.querySelector("[data-output]");
 
-    // Counter
+    setButtonStyle(plusButton);
+    setButtonStyle(minusButton);
+    setButtonStyle(allClearButton);
+
+    // Counter functions
     let number = 0;
+    let displayNumber = function(){
+        output.innerHTML = number;
+    }
     let nowInitialized = function(){
         number = 0;
-        output.innerHTML = number;
+        displayNumber();
     }
     let counterPlus = function(){
         number++;
-        output.innerHTML = number;
+        displayNumber();
     }
     let counterMinus = function(){
         number--;
-        output.innerHTML = number;
+        displayNumber();
     }
 
     plusButton.addEventListener("click", counterPlus);
@@ -47,6 +55,9 @@ function thenDoThis(){
     allClearButton.addEventListener("click",nowInitialized);
 }
 
+function setButtonStyle(elem){
+    elem.style.cssText = 'background-color: #e8e8e8; padding: 0.7em 1.7em; font-size: 18px; border-radius: 0.5em; border: 1px solid #e8e8e8; box-shadow: 6px 6px 12px #c5c5c5,-6px -6px 12px #ffffff; margin: 0.5em'
+}
 
 
 
